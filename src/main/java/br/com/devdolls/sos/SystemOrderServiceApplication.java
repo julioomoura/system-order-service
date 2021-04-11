c67eebd5c91cb7ba6a383175d5da36e0ac06fb56
Júlio <juliomourajom@gmail.com>
2021-04-11 15:56:49 -0300

    :tada: Primeiro commit

---
+++ src/main/java/br/com/devdolls/sos/SystemOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.devdolls.sos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devdolls/sos/SystemOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devdolls.sos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

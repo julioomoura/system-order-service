80e97ffce52c456116f79a8de1b040e82fa8338c
Júlio <juliomourajom@gmail.com>
2021-06-03 01:28:19 -0300

    :recycle: Refatora queries de ordens de serviço

--- src/main/java/br/com/devdolls/sos/repositories/OSRepository.java
+++ src/main/java/br/com/devdolls/sos/repositories/OSRepository.java
@@ -3,23 +3,19 @@ package br.com.devdolls.sos.repositories;
 import br.com.devdolls.sos.entities.OrdemDeServico;
 import br.com.devdolls.sos.entities.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface OSRepository extends JpaRepository<OrdemDeServico, Integer> {
-    List<OrdemDeServico> findAllByDesenvolvedorIdAndClienteIdAndStatus(Integer dev, Integer cliente, Status status);
 
-    List<OrdemDeServico> findByDesenvolvedorIdAndClienteId(Integer dev, Integer cliente);
-
-    List<OrdemDeServico> findByDesenvolvedorIdAndStatus(Integer dev, Status valueOf);
-
-    List<OrdemDeServico> findByDesenvolvedorId(Integer dev);
-
-    List<OrdemDeServico> findAllByClienteIdAndStatus(Integer cliente, Status valueOf);
-
-    List<OrdemDeServico> findAllByClienteId(Integer cliente);
-
-    List<OrdemDeServico> findAllByStatus(Status valueOf);
+    @Query("SELECT o FROM os o WHERE (:dev is null or o.desenvolvedor.id = :dev) and "
+                                + "(:status is null or o.status = :status) and "
+                                + "(:cliente is null or o.cliente.id = :cliente)")
+    List<OrdemDeServico> findAllByDesenvolvedorIdAndClienteIdAndStatus(@Param("dev")Integer dev,
+                                                                       @Param("cliente")Integer cliente,
+                                                                       @Param("status") Status status);
 }

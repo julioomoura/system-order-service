2062f3047077a764cacd5afda91ea9fec1cd1d60
Júlio <juliomourajom@gmail.com>
2021-11-18 22:57:53 -0300

    :rotating_light: Atualiza formatação de classes com spotless

--- src/test/java/br/com/devdolls/sos/services/OSServiceTest.java
+++ src/test/java/br/com/devdolls/sos/services/OSServiceTest.java
@@ -1,5 +1,9 @@
 package br.com.devdolls.sos.services;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
 import br.com.devdolls.sos.dtos.OrdemDeServicoDTO;
 import br.com.devdolls.sos.entities.OrdemDeServico;
 import br.com.devdolls.sos.entities.TipoUsuario;
@@ -7,6 +11,8 @@ import br.com.devdolls.sos.entities.Usuario;
 import br.com.devdolls.sos.entities.enums.Status;
 import br.com.devdolls.sos.repositories.OSRepository;
 import br.com.devdolls.sos.services.os.OSServiceImpl;
+import java.time.LocalDate;
+import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,13 +21,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
-
-import java.time.LocalDate;
-import java.util.Optional;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)

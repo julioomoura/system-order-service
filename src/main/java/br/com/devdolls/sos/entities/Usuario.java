bcddf7d05e924cb1a5cb0503dcaa0c7b4e2fd9e6
Júlio <juliomourajom@gmail.com>
2021-11-19 00:21:13 -0300

    :fire: Remove unused imports

--- src/main/java/br/com/devdolls/sos/entities/Usuario.java
+++ src/main/java/br/com/devdolls/sos/entities/Usuario.java
@@ -7,9 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import java.util.List;
 
 @Entity(name = "usuario")
 @Data
